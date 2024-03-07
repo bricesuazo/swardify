d50a2836b9a5c024fc157914646283ba1db95683
Brice Suazo <bricesuazo@gmail.com>
2024-03-07 19:48:57 +0800

    feat: add tailwind class auto sort

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,14 +5,13 @@ const Home = () => {
   const firstPost = api.firstPost.useQuery();
 
   return (
-    <View className="mt-20 bg-red-500 flex-1 justify-center items-center">
-      <Text className="text-white text-3xl">{firstPost.data?.title}</Text>
-      <View className="web:max-w-[400px] native:mx-3">
-        <Text className="text-white text-md">{firstPost.data?.content}</Text>
+    <View className="mt-20 flex-1 items-center justify-center bg-red-500">
+      <Text className="text-3xl text-white">{firstPost.data?.title}</Text>
+      <View className="native:mx-3 web:max-w-[400px]">
+        <Text className="text-md text-white">{firstPost.data?.content}</Text>
       </View>
-
-      <TouchableOpacity className="p-4 bg-blue-500 rounded-lg">
-        <Text className="text-white">Test</Text>
+      <TouchableOpacity className="rounded-lg bg-blue-500 p-4">
+        <Text className="text-white">Testasd</Text>
       </TouchableOpacity>
     </View>
   );
